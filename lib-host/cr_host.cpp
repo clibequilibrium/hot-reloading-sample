d892e8d8834c60972df985d790d9a0ecea7eeadf
clibequilibrium <skogization@gmail.com>
2022-05-29 21:24:05 -0400

    Initial commit

---
+++ lib-host/cr_host.cpp
@@ -0,0 +1,21 @@
+#define CR_HOST // required in the host only and before including cr.h
+#define CR_DEBUG
+#include <cr.h>
+
+int main(int argc, char *argv[]) {
+  // the host application should initalize a plugin with a context, a plugin
+  cr_plugin ctx;
+
+  // the full path to the live-reloadable application
+  cr_plugin_open(ctx, "f:/Documents/C/hot-reload/build/app/bin/Windows/Debug/libapp.dll");
+
+  // call the update function at any frequency matters to you, this will give
+  // the real application a chance to run
+  while (!cr_plugin_update(ctx)) {
+    // do anything you need to do on host side (ie. windowing and input stuff?)
+  }
+
+  // at the end do not forget to cleanup the plugin context
+  cr_plugin_close(ctx);
+  return 0;
+}
