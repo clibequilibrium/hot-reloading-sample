bef84c5224b6bcf579e423d75ec5d97f255bd7d2
clibequilibrium <105135724+clibequilibrium@users.noreply.github.com>
2022-05-31 23:40:54 -0400

    Added wip flecs sample

--- lib/lib.h
+++ lib/lib.h
@@ -1,6 +1,16 @@
 #ifndef LIB_H
 #define LIB_H
 
-#define SAMPLE_TEXT "Another Sample Text"
+#include <flecs.h>
+
+typedef struct {
+  int x, y;
+} Position, Velocity;
+
+
+extern ECS_COMPONENT_DECLARE(Position);
+extern ECS_COMPONENT_DECLARE(Velocity);
+
+__declspec(dllexport) void Move(ecs_iter_t *it);
 
 #endif
